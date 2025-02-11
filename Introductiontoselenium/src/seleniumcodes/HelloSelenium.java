11f3e0c1d1c54ac75d0e1701454d04534fca8c7d
tommysingh234 <tommysingh3244@gmail.com>
2025-02-11 09:08:07 +0530

    WELCOME TO SELENIUM

---
+++ Introductiontoselenium/src/seleniumcodes/HelloSelenium.java
@@ -0,0 +1,11 @@
+package seleniumcodes;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello SELENIUM");
+
+	}
+
+}
